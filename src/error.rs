4b1d19c423beae8a54cdb640554e3572c5990005
John White <driver@tuk-tuk.io>
2022-08-22 22:19:27 -0400

    remove comment

--- src/error.rs
+++ src/error.rs
@@ -1,24 +1,3 @@
-/*
-type ErrorCode string
-
-const (
-    ErrorCodeAlreadyExist ErrorCode = "value_already_exist"
-    ErrorCodeInvalidValue
-)
-
-type ErrorDetail struct {
-    ErrorCode []ErrorCode `json:"code,omitempty"`
-}
-
-type Error struct {
-    Err error `json:"-"`
-
-    HTTPStatusCode int    `json:"status"`
-    Msg            string `json:"message"`
-
-    ErrorDetail ErrorDetail `json:"error_details"`
-}
-*/
 use serde::{Deserialize, Serialize};
 use std::convert::TryFrom;
 use std::fmt;
